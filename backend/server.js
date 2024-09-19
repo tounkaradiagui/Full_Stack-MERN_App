2dffd97512e5d9c05029649e74a41959d17799a8
diagui <tounkarakan@gmail.com>
2024-09-19 09:39:18 +0000

    Final phase

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import path from 'path';
 dotenv.config();
 import productRoutes from './routes/product.js'
 
@@ -8,13 +9,22 @@ import productRoutes from './routes/product.js'
 
 const app = express();
 app.use(express.json()); //Allows us to accept JSON data in the req.body
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('Backend is working as expected!'); // Send a response back to the client
 });
 
 app.use("/api/products", productRoutes);
 
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+    
+}
 
 // Start the server on port 3000
 app.listen(port, () => {
